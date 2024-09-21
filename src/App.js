4af30a33331ec019c65ca8b9bb70d044c26d4c01
Gracia-Gwati <gracia.gwati@gmail.com>
2024-09-21 15:15:51 +0200

    Reinstalled dependencies

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
+import City from "./City";
+import Weather from "./Weather";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState(null);
+
+  function showWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const apiKey = "203fa770242fcd2b9555d832a88ea567";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(showWeather);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Sky Scout launch</h1>
-      </header>
+    <div>
+      <h1>Sky Scout</h1>
+      <h4>Weather App</h4>
+      <City handleSubmit={handleSubmit} updateCity={setCity} />
+      {loaded && <Weather weather={weather} />}
     </div>
   );
 }
-
-export default App;
